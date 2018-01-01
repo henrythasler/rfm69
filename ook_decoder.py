5062f490111b4fa12cd60a656a82cf7f68e58c76
henrythasler <git@thasler.org>
2018-01-01 18:58:40 +0100

    wip

--- ook_decoder.py
+++ ook_decoder.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 """OOK-Decoder"""
 
+import time
 from lib.rfm69 import Rfm69
 
 def main():
@@ -10,6 +11,9 @@ def main():
         rf.open(0, 32000)
         rx_data = rf.read_register(0x5A)
         print ''.join('0x{:02x} '.format(x) for x in rx_data)
+
+        rf.write_register(0x01, 0b00010000)
+        time.sleep(5)
 
 if __name__ == "__main__":
     main()
